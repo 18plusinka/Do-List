e9e99489fd10e38857a4569d7c317b34cdc71c48
18plusinka <erdemcelik9276@gmail.com>
2025-01-24 08:14:29 +0000

    afa

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import TodoList from './TodoList';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    if (input.trim()) {
+      setTodos([...todos, input]);
+      setInput('');
+    }
+  };
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button onClick={addTodo}>Add</button>
+      <TodoList todos={todos} />
+    </div>
+  );
+}
+
+export default App;
